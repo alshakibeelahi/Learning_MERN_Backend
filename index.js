1114dfef33dd8d3d2766e329aa76ed9c246f2cbe
alshakibeelahi <alshakibeelahi@gmail.com>
2023-09-01 22:31:20 +0600

    routes+controller tested

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port_number = process.env.FRONT_END_PORT_NO
+const testing_routes = require('./routes/test')
 
 app.listen(port_number, ()=>console.log(`This is running on port: ${port_number}`))
 
+
+app.use('/test',testing_routes)
